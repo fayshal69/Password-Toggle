17e9627acb6e11cd1d94ded6eb2b21db38e93006
Fayshal Bin Amir <131062651+fayshal69@users.noreply.github.com>
2023-11-21 18:21:48 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let eyeIcon = document.getElementById("eye-icon");
+let password = document.getElementById("password");
+
+eyeIcon.onclick = function() {
+    if(password.type == "password") {
+        password.type = "text";
+        eyeIcon.src = "images/eye-open.png"
+    }
+    else {
+        password.type = "password";
+        eyeIcon.src = "images/eye-close.png"
+    }
+}
